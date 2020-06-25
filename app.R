e66c58cd2bfed4fe0ad183113b1a6f79bb0505e9
nnnagle <nnnagle@utk.edu>
2020-06-25 08:48:45 -0400

    initial commit

---
+++ app.R
@@ -0,0 +1,84 @@
+library(shiny)
+library(mapdeck)
+
+ui <- fluidPage(
+  tags$head(
+    tags$script(src = "https://api.tiles.mapbox.com/mapbox-gl-js/v1.9.1/mapbox-gl.js"),
+    tags$link(href="https://api.tiles.mapbox.com/mapbox-gl-js/v1.9.1/mapbox-gl.css",
+              rel="stylesheet"),
+    tags$style(HTML("
+      body {
+        margin: 0;
+        padding: 0;
+      }
+      h2,
+      h3 {
+        margin: 10px;
+        font-size: 1.2em;
+      }
+      h3 {
+        font-size: 1em;
+      }
+      p {
+        font-size: 0.85em;
+        margin: 10px;
+        text-align: left;
+      }
+      .map-overlay {
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        background: rgba(255, 255, 255, 0.8);
+        margin-right: 20px;
+        font-family: Arial, sans-serif;
+        overflow: auto;
+        border-radius: 3px;
+      }
+      #map {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        width: 100%;
+      }
+      #features {
+        top: 0;
+        height: 100px;
+        margin-top: 20px;
+        width: 250px;
+      }
+      #legend {
+        padding: 10px;
+        box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+        line-height: 18px;
+        height: 150px;
+        margin-bottom: 40px;
+        width: 100px;
+      }
+      .legend-key {
+        display: inline-block;
+        border-radius: 20%;
+        width: 10px;
+        height: 10px;
+        margin-right: 5px;
+      }")
+    )
+  ),
+  tags$body(
+    tags$div(id="map"),
+    tags$div(class="map-overlay",
+             id="features",
+             tags$h2("Covid-19 population density")),
+    tags$div(class="map-overlay",
+             id="legend"),
+    tags$script(src="map.js"),
+    tags$div(id="map")
+  )
+               
+)
+
+server <- function(input, output) {
+}
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ www/map.js
@@ -0,0 +1,81 @@
+      // define access token
+      mapboxgl.accessToken = 'pk.eyJ1Ijoibm5hZ2xlIiwiYSI6ImNrYmVmdXZxMDBsYW0yeG1ybTF5b2owcDAifQ.gl5PVm5IwBNi7ug63K_39A';
+
+      //create map
+      var map = new mapboxgl.Map({
+        container: 'map', // container id
+        style: 'mapbox://styles/nnagle/ckbtd268g0rlu1io0pvul3kud' // map style URL from Mapbox Studio
+      });
+
+      // wait for map to load before adjusting it
+      map.on('load', function() {
+        // make a pointer cursor
+        map.getCanvas().style.cursor = 'default';
+
+        // set map bounds to the continental US
+        map.fitBounds([
+          [-133.2421875, 16.972741],
+          [-47.63671875, 52.696361]
+        ]);
+
+        // make a pointer cursor
+        map.getCanvas().style.cursor = 'default';
+
+        // define layer names
+        var layers = [
+          '0-10',
+          '10-20',
+          '20-50',
+          '50-100',
+          '100-200',
+          '200-500',
+          '500-1000',
+          '1000+'
+        ];
+        var colors = [
+          '#FFEDA0',
+          '#FED976',
+          '#FEB24C',
+          '#FD8D3C',
+          '#FC4E2A',
+          '#E31A1C',
+          '#BD0026',
+          '#800026'
+        ];
+
+        // create legend
+        for (i = 0; i < layers.length; i++) {
+          var layer = layers[i];
+          var color = colors[i];
+          var item = document.createElement('div');
+          var key = document.createElement('span');
+          key.className = 'legend-key';
+          key.style.backgroundColor = color;
+
+          var value = document.createElement('span');
+          value.innerHTML = layer;
+          item.appendChild(key);
+          item.appendChild(value);
+          legend.appendChild(item);
+        }
+
+        // change info window on hover
+        map.on('mousemove', function(e) {
+          var states = map.queryRenderedFeatures(e.point, {
+            layers: ['statedata']
+          });
+
+          if (states.length > 0) {
+            document.getElementById('pd').innerHTML =
+              '<h3><strong>' +
+              states[0].properties.name +
+              '</strong></h3><p><strong><em>' +
+              states[0].properties.density +
+              '</strong> people per square mile</em></p>';
+          } else {
+            document.getElementById('pd').innerHTML =
+              '<p>Hover over a state!</p>';
+          }
+        });
+      });
+ 
