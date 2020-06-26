a087d0daff31b4936911c20a1a749b9e736403d8
nnnagle <nnnagle@utk.edu>
2020-06-26 19:13:29 -0400

    loads county geojson from github

--- www/map_simple.js
+++ www/map_simple.js
@@ -4,11 +4,19 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoibm5hZ2xlIiwiYSI6ImNrYmVmdXZxMDBsYW0yeG1ybTF5b
 //create map
 var map = new mapboxgl.Map({
   container: 'map', // container id
-  style: 'mapbox://styles/nnagle/ckbvmkkak0aje1ipa576vkr0q' // map style URL from Mapbox Studio
+  //style: 'mapbox://styles/nnagle/ckbvmkkak0aje1ipa576vkr0q', // map style URL from Mapbox Studio
+  style: 'mapbox://styles/mapbox/light-v9',
+  center: [-100, 39],
+  zoom: 3
 });
 
 // wait for map to load before adjusting it
 map.on('load', function() {
+  map.addSource("county", {
+    type: "geojson",
+    data:
+      "https://raw.githubusercontent.com/nnnagle/covid-mapbox/master/counties_data.geojson"
+  });
   // make a pointer cursor
   map.getCanvas().style.cursor = 'default';
   
@@ -18,38 +26,50 @@ map.on('load', function() {
     [-47.63671875, 52.696361]
     ]);
     
+  map.addLayer(
+    {
+     id: "county_layer",
+     type: "fill",
+     source: "county",
+     paint: {
+       "fill-outline-color": "rgba(255,180,255,1)",
+       "fill-color": "rgba(200,0,0,.5)"
+     },
+     "fill-opacity": 1
+    })
+    
   // Create a popup, but don't add it to the map yet.
-  var popup = new mapboxgl.Popup({
-    closeButton: false,
-    closeOnClick: false
-  });  
-  
-  map.on('mouseenter', 'places', function(e) {
-    // Change the cursor style as a UI indicator.
-    map.getCanvas().style.cursor = 'pointer';
-    
-    var coordinates = e.features[0].geometry.coordinates.slice();
-    var description = e.features[0].properties.NAME;
- 
-    // Ensure that if the map is zoomed out such that multiple
-    // copies of the feature are visible, the popup appears
-    // over the copy being pointed to.
-    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-      coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-    }
-    
-    // Populate the popup and set its coordinates
-    // based on the feature found.
-    popup
-    .setLngLat(coordinates)
-    .setHTML(description)
-    .addTo(map);
-  });
-  
-  map.on('mouseleave', 'places', function() {
-    map.getCanvas().style.cursor = '';
-    popup.remove();
-  });
+//  var popup = new mapboxgl.Popup({
+//    closeButton: false,
+//    closeOnClick: false
+//  });  
+//  
+//  map.on('mouseenter', 'places', function(e) {
+//    // Change the cursor style as a UI indicator.
+//    map.getCanvas().style.cursor = 'pointer';
+//    
+//    var coordinates = e.features[0].geometry.coordinates.slice();
+//    var description = e.features[0].properties.NAME;
+// 
+//    // Ensure that if the map is zoomed out such that multiple
+//    // copies of the feature are visible, the popup appears
+//    // over the copy being pointed to.
+//    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+//      coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+//    }
+//    
+//    // Populate the popup and set its coordinates
+//    // based on the feature found.
+//    popup
+//    .setLngLat(coordinates)
+//    .setHTML(description)
+//    .addTo(map);
+//  });
+//  
+//  map.on('mouseleave', 'places', function() {
+//    map.getCanvas().style.cursor = '';
+//    popup.remove();
+//  });
 });
 
 
