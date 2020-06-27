7241a1ba7d29fa65f3dc74e245ef438b23e6024e
nnnagle <nnnagle@utk.edu>
2020-06-26 21:05:00 -0400

    added popup on mouseover

--- www/map_simple.js
+++ www/map_simple.js
@@ -52,37 +52,48 @@ map.on('load', function() {
     })
     
   // Create a popup, but don't add it to the map yet.
-//  var popup = new mapboxgl.Popup({
-//    closeButton: false,
-//    closeOnClick: false
-//  });  
-//  
-//  map.on('mouseenter', 'places', function(e) {
-//    // Change the cursor style as a UI indicator.
-//    map.getCanvas().style.cursor = 'pointer';
-//    
-//    var coordinates = e.features[0].geometry.coordinates.slice();
-//    var description = e.features[0].properties.NAME;
-// 
-//    // Ensure that if the map is zoomed out such that multiple
-//    // copies of the feature are visible, the popup appears
-//    // over the copy being pointed to.
-//    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-//      coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-//    }
-//    
-//    // Populate the popup and set its coordinates
-//    // based on the feature found.
-//    popup
-//    .setLngLat(coordinates)
-//    .setHTML(description)
-//    .addTo(map);
-//  });
-//  
-//  map.on('mouseleave', 'places', function() {
-//    map.getCanvas().style.cursor = '';
-//    popup.remove();
-//  });
+  var popup = new mapboxgl.Popup({
+    closeButton: false,
+    closeOnClick: false
+  });  
+  
+  map.on('mousemove', 'county_layer', function(e) {
+    popup.remove();
+    // Change the cursor style as a UI indicator.
+    map.getCanvas().style.cursor = 'pointer';
+    
+    var displayStr;
+    
+    if ( (e.features[0].properties[date] != 'null')) {
+      displayStr =
+        e.features[0].properties.NAME +
+        " County " +
+        "(" +
+        date +
+        ")<br>Rate: " +
+        e.features[0].properties[date];
+    } else {
+      displayStr =
+        e.features[0].properties.NAME+
+        " County " +
+        "(" +
+        date +
+        ")<br>Rate: No Estimate";
+    }
+ 
+    // Populate the popup and set its coordinates
+    // based on the feature found.
+    popup
+    .setLngLat(e.lngLat)
+    .setHTML(displayStr)
+    .addTo(map);
+  });
+  
+  // change it back to a pointer when it leaves
+  map.on('mouseleave', 'county_layer', function() {
+    map.getCanvas().style.cursor = '';
+    popup.remove();
+  });
 });
 
 
