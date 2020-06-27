5fb67a07c9f48246b6e09ff0bcc67bbd6578ef99
nnnagle <nnnagle@utk.edu>
2020-06-27 08:49:13 -0400

    figured out mapbox with bootstrap

--- app.R
+++ app.R
@@ -11,23 +11,39 @@ ui <- fluidPage(
         margin: 0;
         padding: 0;
       }
-      #map-containter {
-        position: inherited;
-        left:400px;
-        height: 600px;
-        width: 400px;
+      #map-container {
+        border: 2px solid blue;
+        position: relative;
+        z-index: 1;
+        height: 400px;
+        width: 100%;
       }
       #map {
         border: 2px solid black;
-        left: 400px;
-        height: 400px;
-        margin-bottom: 10px;
+        z-index: 1;
+        width: 100%;
+        height: 100%;
       }
       #map img {
         max-width: none;
         min-width: 0px;
         height: auto;
-}
+      }
+      .session-new{
+/*   display: flex;
+        //flex-direction: column; */
+        position: absolute;
+        width: 340px;
+        left: 33%;
+        top: 25px;
+        //left: 500px;
+        z-index: 2;
+        padding: 20px;
+        border-radius: 4px;
+        //margin-top: 4vh;
+        background-color: rgba(255, 255, 255, 0.85);
+      }
+
       ")
     )
   ),
@@ -35,11 +51,21 @@ ui <- fluidPage(
   fluidRow(h2('Row0')),
   tags$div(class='row',
            tags$h2("Row 1")),
-  tags$div(class='row', style="border: 2px solid black; height: 500px",
-    tags$div(class="col", tags$h2("Text")),
-    tags$div(class="col",
+  tags$div(class='row', style="border: 2px solid black; height: 500px;",
+    tags$div(class="col-lg-3", style="border: 2px solid black; ",tags$h2("Text")),
+    tags$div(class="col-lg-9", style="border: 2px solid black; ",
       tags$div(id='map-container',
-          tags$div(id="map")
+          tags$div(id="map"),
+          tags$div(id='sliderbar',
+                   class='session-new',
+                   tags$p('Slider'),
+                   tags$input(id="slider-new",
+                              class='row-new',
+                              type='range',
+                              min='0',
+                              max='14',
+                              step='1',
+                              value='0')),
       ),
       tags$script(src="map_simple.js")
     ) #close col
